54bab844b0447dfded612f5b0fbfe7970d985dd5
Andrew Spiers <andrew@andrewspiers.net>
2013-11-10 09:13:46 +1100

    Use a positional argument instead of optional args

--- psinfo/tomcatinfo.py
+++ psinfo/tomcatinfo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-"""Return some information about the tomcat process.
+"""Return the pid, number of threads, or percentage of physical memory used
+by any tomcat process running on the system.
 
-Usage: 
 """
 
 import psutil
@@ -25,19 +25,8 @@ def findtomcat():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = __doc__ )
     parser.add_argument(
-        "--pid",
-        action='store_true',
-        help = "return the pid of the process."
-        )
-    parser.add_argument(
-        "--threads",
-        action='store_true',
-        help = "return the number of threads of the process."
-        )
-    parser.add_argument(
-        "--memper",
-        action='store_true',
-        help = "return the percentage the processes's RSS as a percentage of system memory."
+        "attribute",
+        choices=['threads','pid','memper'],
         )
     args = parser.parse_args()
     toms = findtomcat()
@@ -48,13 +37,9 @@ if __name__ == "__main__":
         sys.stderr.write('More than one tomcat process found.')
         raise IndexError
     tom = toms[0]
-    #if not args.attribute:
-    #    print 'tomcat pid is ' + str(tom.pid)
-    #    print 'thread count :',tom.get_num_threads()
-    #    print 'rss / physical mem :',tom.get_memory_percent()
-    if args.pid:
+    if args.attribute == "pid":
         print tom.pid
-    if args.threads:
+    if args.attribute == "threads":
         print tom.get_num_threads()
-    if args.memper:
+    if args.attribute == "memper":
         print tom.get_memory_percent()
