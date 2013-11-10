9f291f9ec37ec6ca5e9a540c395c68f45aa136ab
Andrew Spiers <andrew@andrewspiers.net>
2013-11-10 20:56:55 +1100

    added check for absolute memory.

---
+++ psinfo/absolutememorynagios.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+"""NRPE check for available (free + buffers + cache) memory.
+
+Generates a warning state if the available physical memory is less than  -w
+(default 512MB), and a critical state if the physical memory exceeds -c
+(default 256MB).
+"""
+
+import psutil
+
+import argparse
+import sys
+
+MB = 1024**2  #bytes in a Megabyte
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-w",
+        type=int,
+        default=512,
+        help='warning level',
+        )
+    parser.add_argument(
+        "-c",
+        type=int,
+        default=256,
+        help='critical level',
+        )
+    args = parser.parse_args()
+
+    #have to use avail_phymem to support psutils v 0.5 in debian wheezy.
+    physmem = psutil.avail_phymem() / MB  #available physical mem in megabytes.
+    if args.c >= args.w:
+        print ('UNKNOWN: critical level must be less than warning level')
+        sys.exit(3)
+    if physmem > args.w:
+        print ("OK - %#s megabytes of physical memory available." % physmem)
+        sys.exit(0)
+    elif physmem > args.c:
+        print ("WARNING - %#s megabytes of physical memory available." %
+                physmem)
+        sys.exit(1)
+    elif physmem <= args.c:
+        print ("CRITICAL - %#s megabytes of physical memory available." %
+                physmem)
+        sys.exit(2)
+    else:
+        print ("UNKNOWN - %#s megabytes of physical memory used. " % physmem +
+               "Something is awry.")
+        sys.exit(3)
